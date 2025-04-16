2cd9040c36731eb88a8d4c479b6c7349ca4fe34d
Donny Hansen <dxhans5@gmail.com>
2025-04-15 22:22:51 -0600

    initial commit

---
+++ application/resources/js/app.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+
+import ExampleComponent from './components/ExampleComponent.vue'
+
+const app = createApp()
+app.component('example-component', ExampleComponent)
+app.mount('#app');
